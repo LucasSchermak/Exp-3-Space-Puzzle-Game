b89fef6b2afe7c619f92c207602221e888da1821
LucasGschermak <lgschermak@gmail.com>
2020-03-23 09:41:10 -0300

    Create CameraController.cs

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    [Header("Player list")]
+    public List<Transform> targets;
+    [Space]
+    public Vector3 offset;
+    public float smoothTime = .5f;
+    [Space]
+    [Header("Camera Zoom")]
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+    
+    private Vector3 velocity;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        FovZoom();
+    }
+
+    void FovZoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance()/100f);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom,Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+    void Move()
+    {
+        Vector3 centerpoint = GetCenterPoint();
+        Vector3 newPosition = centerpoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+
+    }
+}
