628455a5d2f7e60df1993813a26d4f539b3a6192
LucasGschermak <lgschermak@gmail.com>
2020-03-23 09:41:25 -0300

    Create GameManagerController.cs

---
+++ Assets/Script/GameManagerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+    public static GameManagerController Instance;
+    public bool interagindo1 = false;
+    public bool interagindo2 = false;
+    public bool onButton1 = false;
+    public bool onButton2 = false;
+    public bool tudoJunto = false;
+ 
+    private void Start()
+    {
+
+        if (Instance == null)             
+            Instance = this;
+    }
+    private void Update()
+    {
+        if (onButton1 && onButton2)
+        {
+            Interact(interact1: interagindo1, interact2: interagindo2);
+        }
+    }
+    public void Interact(bool interact1, bool interact2)
+    {
+         if (interact1 && interact2)
+         {
+             Debug.Log("funciona");
+             tudoJunto = true;
+         }else
+             Debug.Log("não funciona");
+    }
+    
+
+}
+
+
