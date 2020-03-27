87b9a148330f666cfed438a41a22cb702a27e94b
LucasGschermak <lgschermak@gmail.com>
2020-03-27 20:11:04 -0300

    Update PlayerControl.cs

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 public class PlayerControl : MonoBehaviour
 {
-    public GameManagerController _gameController;
+    public GameManagerController gameController;
     public CaracterController2D controller;
     public GameObject current;
 
@@ -20,64 +21,15 @@ public class PlayerControl : MonoBehaviour
     
     void Start()
     {
-        _gameController = GameManagerController.Instance;
-        StartCoroutine(ResetButtons());
-        
+        gameController = GameManagerController.instance;
     }
-
-    private IEnumerator ResetButtons()
-    {
-        if (_gameController.interagindo1 == true)
-            {
-                Debug.Log("start coouroutine");
-                yield return new WaitForSeconds(100*Time.deltaTime);
-                _gameController.interagindo1 = false;
-                Debug.Log("end couroutine");
-
-            }
-
-            if (_gameController.interagindo2 == true)
-            {
-                Debug.Log("start coouroutine");
-                yield return new WaitForSeconds(100*Time.deltaTime);
-                _gameController.interagindo2 = false;
-                Debug.Log("end couroutine");
-            }
-    }
-
     void Update()
     {
-        if (current.tag == "Player1")
+        horizontalMove = Input.GetAxisRaw("Horizontal.P1") * runSpeed;
+
+        if (Input.GetButtonDown("Jump.P1"))
         {
-            horizontalMove = Input.GetAxisRaw("Horizontal.P1") * runSpeed;
-
-            if (Input.GetButtonDown("Jump.P1"))
-            {
-                jump = true;
-            }
-
-            if (Input.GetButton("Interactive.P1"))
-            {
-                _gameController.interagindo1 = true;
-                Debug.Log("botão 1");
-            }
-
-        }
-
-        if (current.tag == "Player2")
-        {
-            horizontalMove = Input.GetAxisRaw("Horizontal.P2") * runSpeed;
-
-            if (Input.GetButtonDown("Jump.P2"))
-            {
-                jump = true;
-            }
-
-            if (Input.GetButton("Interactive.P2"))
-            {
-                _gameController.interagindo2 = true;
-                Debug.Log("botão 2");
-            }
+            jump = true;
         }
 
         if (Input.GetButtonDown("Crouch"))
@@ -94,8 +46,6 @@ public class PlayerControl : MonoBehaviour
     {
         controller.Move(move: horizontalMove * Time.fixedDeltaTime, crouch: false, jump: jump);
         jump = false;
-        StartCoroutine(ResetButtons());
-        //ResetButtonsFalse();
     }
     
 }
