293c18f1023cdbde40d14d630363e0fa590bce5a
LucasGschermak <lgschermak@gmail.com>
2020-03-27 20:10:59 -0300

    Update GameManagerController.cs

--- Assets/Script/GameManagerController.cs
+++ Assets/Script/GameManagerController.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 
 public class GameManagerController : MonoBehaviour
 {
-    public static GameManagerController Instance;
-    public bool interagindo1 = false;
-    public bool interagindo2 = false;
-    public bool onButton1 = false;
-    public bool onButton2 = false;
-    public bool tudoJunto = false;
+    public static GameManagerController instance;
  
     private void Start()
     {
-
-        if (Instance == null)             
-            Instance = this;
+        if (instance == null)             
+            instance = this;
     }
+
     private void Update()
     {
-        if (onButton1 && onButton2)
-        {
-            Interact(interact1: interagindo1, interact2: interagindo2);
-        }
     }
-    public void Interact(bool interact1, bool interact2)
-    {
-         if (interact1 && interact2)
-         {
-             Debug.Log("funciona");
-             tudoJunto = true;
-         }else
-             Debug.Log("não funciona");
-    }
-    
 
 }
 
