5a4731c6d9c5965b50c9c734f90e03af40480fe6
LucasGschermak <lgschermak@gmail.com>
2020-03-27 20:10:50 -0300

    Create FlashLight.cs

---
+++ Assets/Script/FlashLight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    private bool flashLightEnabled;
+    public GameObject flashLight;
+    public GameObject lightObj;
+    public float maxEnergy;
+    private float currentEnergy;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashLightEnabled =! flashLightEnabled;
+        }
+        flashLight.SetActive(flashLightEnabled);
+    }
+}
