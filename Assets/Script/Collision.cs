ebe7f452a6150ea2f6353b8f77e5514e629787c1
LucasGschermak <lgschermak@gmail.com>
2020-03-30 09:54:25 -0300

    Update Collision.cs

--- Assets/Script/Collision.cs
+++ Assets/Script/Collision.cs
@@ -12,17 +12,13 @@ public class Collision : MonoBehaviour
     [Space]
 
     public bool onGround;
-    public bool onWall;
-    public bool onRWall;
-    public bool onLWall;
-    public bool cellingCheck;
-    public int wallSide;
-    public bool seeThroughWall;
+    [SerializeField]private bool onWall;
+    [SerializeField]private bool cellingCheck;
+    [SerializeField]private bool seeThroughWall;
     [Space] [Header("Collision")] 
     
-
-    public float collisionRadius = 0.25f;
-    public Vector2 bottomOffset, rightOffset, leftOffset, upOffset;
+    [SerializeField]private float collisionRadius = 0.25f;
+    [SerializeField]private Vector2 bottomOffset, upOffset;
     void Start()
     {
         
@@ -31,23 +27,16 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
         cellingCheck = Physics2D.OverlapCircle((Vector2)transform.position + upOffset, collisionRadius, groundLayer);
-        onWall = Physics2D.OverlapCircle((Vector2) transform.position + rightOffset , collisionRadius,
-            groundLayer)||Physics2D.OverlapCircle((Vector2) transform.position + leftOffset , collisionRadius,
-                     groundLayer);
-        wallSide = onRWall ? -1 : 1;
+        
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        var positions = new Vector2[]{bottomOffset,rightOffset,leftOffset};
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
         Gizmos.DrawWireSphere((Vector2)transform.position + upOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
         Gizmos.DrawRay(transform.position,direction: Vector3.back);
     }
 }
